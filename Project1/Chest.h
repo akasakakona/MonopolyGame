45ee70517c0164856509afa7af70b36d1cf14906
ryanle432 <ryanle2334@gmail.com>
2021-03-06 18:14:08 -0800

    Added interacts for chance and community chest cards

--- Project1/Chest.h
+++ Project1/Chest.h
@@ -3,11 +3,12 @@
 
 #include<iostream>
 #include"Property.h"
+#include"ChessPiece.h"
 using namespace std;
 
 class Chest : public Property{
     public:
-	Chest(int price, int rent, int mortgage, char attribute, string name, int ID){
+	Chest(int price, int rent, int mortgage, char attribute, string name, int ID, vector<ChessPiece*> chestCards){
 		this->price = price;
 		this->rent = rent;
 		this->mortgage = mortgage;
@@ -15,6 +16,7 @@ class Chest : public Property{
 		purchasable = false;
 		this->name = name;
 		this->ID = ID;
+		this->chestCards = chestCards;
 	}
     ~Chest(){}
     
@@ -42,6 +44,84 @@ class Chest : public Property{
 	virtual bool is_purchasable(){
 		return this->purchasable;
 	}
+	virtual void interact(Player* currPlayer) {
+		//Generate random int
+		srand(time(NULL));
+		int randomInt = rand() % this->chestCards.size();
+		//Print out the card that was given
+		cout << "Your card says... " << chestCards.at(randomInt)->getName() << endl;
+
+		if (chestCards.at(randomInt)->getID() == 0) {
+			currPlayer->set_position(0);
+			cout << "You get $200" << endl;
+			currPlayer->change_money(200);
+		}
+		else if (chestCards.at(randomInt)->getID() == 1) {
+			//Collect $200
+			currPlayer->change_money(200);
+		}
+		else if (chestCards.at(randomInt)->getID() == 2) {
+			//Doctor fees
+			currPlayer->change_money(-50);
+		}
+		else if (chestCards.at(randomInt)->getID() == 3) {
+			//Stonks
+			currPlayer->change_money(50);
+		}
+		else if (chestCards.at(randomInt)->getID() == 4) {
+			//Get out of jail
+			cout << "Get out of jail is added to your inventory" << endl;
+			currPlayer->change_jail_card(1);
+		}
+		else if (chestCards.at(randomInt)->getID() == 5) {
+			//Go to jail
+			cout << "You've been jailed!" << endl;
+			currPlayer->set_jailed(true);
+		}
+		else if (chestCards.at(randomInt)->getID() == 6) {
+			//Opera
+			currPlayer->change_money(50);
+		}
+		else if (chestCards.at(randomInt)->getID() == 7) {
+			//Holiday fund
+			currPlayer->change_money(100);
+		}
+		else if (chestCards.at(randomInt)->getID() == 8) {
+			//Income tax refund
+			currPlayer->change_money(20);
+		}
+		else if (chestCards.at(randomInt)->getID() == 9) {
+			//Birthday
+			currPlayer->change_money(10);
+		}
+		else if (chestCards.at(randomInt)->getID() == 10) {
+			//Life insurance
+			currPlayer->change_money(100);
+		}
+		else if (chestCards.at(randomInt)->getID() == 11) {
+			//Hospital fees
+			currPlayer->change_money(-50);
+		}
+		else if (chestCards.at(randomInt)->getID() == 12) {
+			//School fees
+			currPlayer->change_money(-50);
+		}
+		else if (chestCards.at(randomInt)->getID() == 13) {
+			//Consulting fees
+			currPlayer->change_money(50);
+		}
+		else if (chestCards.at(randomInt)->getID() == 14) {
+			//Beauty contest
+			currPlayer->change_money(10);
+		}
+		else if (chestCards.at(randomInt)->getID() == 15) {
+			//Inherit 100
+			currPlayer->change_money(100);
+		}
+	}
+
+	private: 
+		vector<ChessPiece*> chestCards;
 };
 
 
