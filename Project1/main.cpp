867e18bfe8d0fb0a017429bd290ace943daadb05
akasakakona <tomliang20011018@gmail.com>
2021-03-08 01:41:36 -0800

    Updated main.cpp to remove the loop

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    while(argc != 3){
-        cout << "Incorrect Usage! The proper usage should be: \n \'./ThisGame <map_config_filename> <card_config_filename>";
+    if(argc != 3){
+        cout << "Incorrect Usage! The proper usage should be: \n \'./ThisGame <map_config_filename> <card_config_filename>\'" << endl;
+        return 0;
     }
     Game g1(argv[1], argv[2]);
     g1.run();
