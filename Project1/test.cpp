73dbde36ed53d6bf0dbc0ae91c1235bc6668eb52
ryanle432 <ryanle2334@gmail.com>
2021-03-07 22:21:15 -0800

    Created tests

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -2,6 +2,7 @@
 #include "unit_test/PropertyFactory_test.hpp"
 #include "unit_test/Property_test.h"
 #include "unit_test/Player_test.hpp"
+#include "unit_test/Card_test.hpp"
 
 
 int main(int argc, char **argv) {
